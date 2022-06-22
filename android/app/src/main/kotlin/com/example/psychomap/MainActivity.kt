56324f63d54fac43aad7b3322870e729ecb776dc
DevaPrakashNair <devaprakashnair@gmail.com>
2022-06-22 20:54:29 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/psychomap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.psychomap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title of the "),
+      ),
+      drawer: Drawer(
+        child: Container(
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
