d0b360d913218f550b3a34c411e5136150fe37fe
DevaPrakashNair <devaprakashnair@gmail.com>
2022-06-22 21:10:52 +0530

    123

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_widget/google_maps_widget.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -11,17 +11,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Title of the "),
-      ),
-      drawer: Drawer(
-        child: Container(
-          child: Column(
-            children: [
-
-            ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Title of the "),
+        ),
+        drawer: Drawer(
+          child: Container(
+            child: Column(
+              children: [
+                Text("Home"),
+                Divider(),
+                Text("Review"),
+                Divider(),
+                Text("Map"),
+                Divider(),
+              ],
+            ),
           ),
+        ),
+        body: GoogleMapsWidget(
+          apiKey: "YOUR KEY HERE",
+          sourceLatLng: LatLng(40.484000837597925, -3.369978368282318),
+          destinationLatLng: LatLng(40.48017307700204, -3.3618026599287987),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'homePage.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
   }
 }
